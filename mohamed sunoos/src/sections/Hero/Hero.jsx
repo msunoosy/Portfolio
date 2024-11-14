4bc05af989bbcca1de64222378fcaf6c94231350
zamzamy Mohamed sunoosy <m.sunoos@gmail.com>
2024-11-15 02:38:10 +0400

    them button imported

--- mohamed sunoos/src/sections/Hero/Hero.jsx
+++ mohamed sunoos/src/sections/Hero/Hero.jsx
@@ -1,15 +1,18 @@
 import styles from './HeroStyles.module.css'
 import heroImg from "../../assets/hero-img.png"
+import themeIcon from "../../assets/sun.svg"
 
 
 
 function Hero() {
-  return <section id='hero'>
-    <div>
-      <img src={heroImg} alt=''></img>
+  return <section id='hero' className={styles.container} >
+    <div className={styles.classModeContainer}>
+      <img className={styles.hero} src={heroImg} alt='Profile pic of sunoos'></img>
+
+      <img src={themeIcon}>
+      </img>
     </div>
     </section>
-  
 }
 
 export default Hero
